f72d3f83eafa766fcdc0225db32ff39648efaa5a
tchevaphatra <thaya.chevaphatrakul@student.unimelb.edu.au~>
2023-03-09 01:30:20 +1100

    updated ui

--- bingo/src/component/Main.js
+++ bingo/src/component/Main.js
@@ -5,7 +5,7 @@ export default function Main(props) {
         <section id="main-menu">
             <div className="main-container">
                 <div className="menu-header">
-                    <h1>ULTIMATE<span>BINGO</span></h1>
+                    <h1>CASUAL<span>BINGO</span></h1>
                     <p>Welcome to bingo, win by covering a complete horizontal, vertical or diagonal line. This game requires 2 people to play.</p>
                 </div>
                 <div className="menu-buttons">
@@ -13,40 +13,43 @@ export default function Main(props) {
                 </div>
             </div>
             <div className="bingo-logo">
-                <h1 className="logo-text">
-                    <span>Let's</span>
-                    <span>Play</span>
-                    <span>Bingo</span>
-                </h1>
-                <div className="circle"></div>
-                <div className="card">
-                    <div className="card-grid">
-                        <div className="grid-items"></div>
-                        <div className="grid-items"></div>
-                        <div className="grid-items"></div>
-                        <div className="grid-items"></div>
-                        <div className="grid-items"></div>
-                        <div className="grid-items"></div>
-                        <div className="grid-items"></div>
-                        <div className="grid-items"></div>
-                        <div className="grid-items"></div>
-                        <div className="grid-items"></div>
-                        <div className="grid-items"></div>
-                        <div className="grid-items"></div>
-                        <div className="grid-items"></div>
-                        <div className="grid-items"></div>
-                        <div className="grid-items"></div>
-                        <div className="grid-items"></div>
-                        <div className="grid-items"></div>
-                        <div className="grid-items"></div>
-                        <div className="grid-items"></div>
-                        <div className="grid-items"></div>
-                        <div className="grid-items"></div>
-                        <div className="grid-items"></div>
-                        <div className="grid-items"></div>
-                        <div className="grid-items"></div>
-                        <div className="grid-items"></div>
+                <div className="curve"></div>
+                <div className="circle">
+                    <h1 className="logo-text">
+                        <span>Let's</span>
+                        <span>Play</span>
+                        <span>Bingo</span>
+                    </h1>
+                    <div className="card">
+                        <div className="card-grid">
+                            <div className="grid-items"></div>
+                            <div className="grid-items"></div>
+                            <div className="grid-items"></div>
+                            <div className="grid-items"></div>
+                            <div className="grid-items"></div>
+                            <div className="grid-items"></div>
+                            <div className="grid-items"></div>
+                            <div className="grid-items"></div>
+                            <div className="grid-items"></div>
+                            <div className="grid-items"></div>
+                            <div className="grid-items"></div>
+                            <div className="grid-items"></div>
+                            <div className="grid-items"></div>
+                            <div className="grid-items"></div>
+                            <div className="grid-items"></div>
+                            <div className="grid-items"></div>
+                            <div className="grid-items"></div>
+                            <div className="grid-items"></div>
+                            <div className="grid-items"></div>
+                            <div className="grid-items"></div>
+                            <div className="grid-items"></div>
+                            <div className="grid-items"></div>
+                            <div className="grid-items"></div>
+                            <div className="grid-items"></div>
+                            <div className="grid-items"></div>
+                        </div>
                     </div>
+                    <div className="inner-circle"></div>
                 </div>
             </div>
         </section>
